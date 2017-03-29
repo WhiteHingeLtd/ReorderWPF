5057a3c1897af577bb6c1c72b8669ccfa8f5462e
OwenH <OwenH@whitehinge.com>
2017-03-29 10:41:19 +0100

    asd

--- ReorderWPF/MainWindow.xaml.cs
+++ ReorderWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Deployment.Internal;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WHLClasses;
 
 namespace ReorderWPF
 {
@@ -20,9 +22,21 @@ namespace ReorderWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public EmployeeCollection Data_Employees;
+        internal WHLClasses.Authentication.AuthClass User_Employee;
+        internal SkuCollection Data_Skus;
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            var Splash = new Splash();
+            Splash.HomeRef = this;
+            Splash.InitializeComponent();
+            Splash.ShowDialog();
         }
     }
 }
---
+++ ReorderWPF/Splash.xaml.cs
@@ -0,0 +1,84 @@
+﻿
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+using WHLClasses;
+using WHLClasses.Authentication;
+
+namespace ReorderWPF
+{
+    /// <summary>
+    /// Interaction logic for Splash.xaml
+    /// </summary>
+    public partial class Splash : Window
+    {
+        BackgroundWorker Worker = new BackgroundWorker();
+        internal MainWindow HomeRef = null;
+        internal void SplashLoad()
+        {
+            Worker.DoWork += SplashProxy;
+            Worker.ProgressChanged += ProxyTextUpdate;
+            Worker.RunWorkerCompleted += ProxyFinished;
+            Worker.WorkerReportsProgress = true;
+            Worker.RunWorkerAsync();
+        }
+
+        private void LoadAssemblies(Assembly Assembly)
+        {
+            foreach (AssemblyName name in Assembly.GetReferencedAssemblies())
+            {
+                Worker.ReportProgress(0, "Loading " + name.Name.ToString());
+                Thread.Sleep(25);
+                if (!AppDomain.CurrentDomain.GetAssemblies().Any((Assembly x) => x.FullName == name.FullName))
+                {
+                    LoadAssemblies(Assembly.Load(name));
+                }
+
+            }
+        }
+
+        internal void SplashProxy(object sender, DoWorkEventArgs e)
+        {
+            Worker.ReportProgress(0, "Loading Assemblies");
+            LoadAssemblies(this.GetType().Assembly);
+            GenericDataController loader = new GenericDataController();
+            Worker.ReportProgress(0, "Loading Item Data");
+            HomeRef.Data_Skus = loader.SmartSkuCollLoad();
+            Worker.ReportProgress(0, "Loading Employee Data");
+            HomeRef.Data_Employees = new EmployeeCollection();
+            Worker.ReportProgress(0, "Preparing Authentication");
+            HomeRef.User_Employee = new AuthClass();
+
+        }
+
+        private void UpdateText(string NewText)
+        {
+            LoadingStatsText.Text += Environment.NewLine + NewText;
+        }
+
+        internal void ProxyTextUpdate(object sender, ProgressChangedEventArgs e)
+        {
+            UpdateText(e.UserState.ToString());
+        }
+
+        internal void ProxyFinished(object sender, RunWorkerCompletedEventArgs e)
+        {
+            UpdateText("Requesting User Login");
+            UpdateText("Logged in: " + HomeRef.User_Employee.AuthenticatedUser.FullName);
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SplashLoad();
+        }
+    }
+}
